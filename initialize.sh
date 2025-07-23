986c2f76119920373c72c0a0534dd7e665791937
Vincent Danen <vdanen@redhat.com>
2025-07-23 14:28:18 -0600

    a simple script to download the latest VEX archive and create the db

---
+++ initialize.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+
+archive=$(curl https://security.access.redhat.com/data/csaf/v2/vex/archive_latest.txt)
+today="vex-$(date +%Y-%m-%d)"
+
+curl -OL https://security.access.redhat.com/data/csaf/v2/vex/${archive}
+
+mkdir ${today}
+tar xf ${archive} -C ${today}
+
+sqlite3 vex.db <vex-db.sql
+
+time python3 import-vex.py ${today} --database-url "sqlite:///vex.db"
